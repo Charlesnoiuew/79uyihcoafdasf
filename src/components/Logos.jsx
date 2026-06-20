35e318f52455fd343fd5ab77842dc48e3e3b646d
rehgsh <105461763+Charlesnoiuew@users.noreply.github.com>
2026-06-19 22:31:22 -0500

    Refactor Logos component

--- src/components/Logos.jsx
+++ src/components/Logos.jsx
@@ -1,19 +1,21 @@
 import './Logos.css'
 
-const IMG = '/images/image copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy copy.png'
+const logos = [
+  { src: '/images/png-transparent-atlantic-records-hd-logo.png', alt: 'Atlantic Records' },
+  { src: '/images/united-masters-review-930x562-1.webp', alt: 'United Masters' },
+  { src: '/images/Dream-Chasers-Records-Logo.jpg', alt: 'Dream Chasers Records' },
+]
 
 export default function Logos() {
   return (
     <div className="logos-strip">
       <div className="logos-strip__label">Worked With</div>
-      <div className="logos-strip__crop">
-        <img src={IMG} alt="Companies worked with" className="logos-strip__img" />
-      </div>
-      <div className="logos-strip__extra">
-        <img src="/images/united-masters-review-930x562-1.webp" alt="United Masters" className="logos-strip__extra-img" />
-      </div>
-      <div className="logos-strip__extra">
-        <img src="/images/Dream-Chasers-Records-Logo.jpg" alt="Dream Chasers Records" className="logos-strip__extra-img" />
+      <div className="logos-strip__list">
+        {logos.map(({ src, alt }) => (
+          <div key={alt} className="logos-strip__item">
+            <img src={src} alt={alt} className="logos-strip__img" />
+          </div>
+        ))}
       </div>
     </div>
   )
